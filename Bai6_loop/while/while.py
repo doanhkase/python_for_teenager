4bccca0a1aa0d3571cb8fd41bec7f396f2fa3bda
doanhkase <70676082+doanhkase@users.noreply.github.com>
2020-09-03 09:51:19 +0700

    while

---
+++ Bai6_loop/while/while.py
@@ -0,0 +1,12 @@
+# Bài tập hiểu vòng lặp while trong python
+
+# tạo biến i
+i = 0
+
+# bắt đầu vòng lặp while
+while i < 5: # i = 0 ->4
+    # code trong vòng lặp while
+    print('Số hiện tại là: ', i)
+    i = i + 1
+
+print('Thoát while')
